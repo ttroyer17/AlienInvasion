950a184549ac882089e8c47d9f562e1c88207522
ttroyer <ttroyer17@gmail.com>
2026-07-29 21:08:15 -0400

    refactored Alien Invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -15,24 +16,30 @@ class AlienInvasion:
             self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game to run."""
         while True:
             # Watch for keyboard and mouse events.
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
+            """Respond to keypresses and mouse events."""
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            # Redraw the screen during each pass through the loop.
-            # Uses screen fill to setup background color from settings.py
-            self.screen.fill(self.settings.bg_color)
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
-            # Make the most recently drawn screen visable.
-            pygame.display.flip()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()    
 
 if __name__ == '__main__':
-    # Make a game instance, and  run the game.
+    # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
--- ship.py
+++ ship.py
@@ -3,7 +3,7 @@ import pygame
 class Ship:
     """A class to manage the ship."""
     def __init__(self, ai_game):
-        """Initialize teh ship and set its starting point."""
+        """Initialize the ship and set its starting point."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
@@ -12,8 +12,8 @@ class Ship:
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
-        self.rect.mdbottom = self.screen_rect.midbottom
+        self.rect.midbottom = self.screen_rect.midbottom
 
-    def bnlitme(self):
+    def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
